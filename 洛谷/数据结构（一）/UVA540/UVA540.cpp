2d6b37fe7fee2b05b8ba2a2db6695c56496b78ad
ccccrrrr <zcr1006@gmail.com>
2021-08-17 13:47:39 +0800

    .

---
+++ 洛谷/数据结构（一）/UVA540/UVA540.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <algorithm>
+#include <deque>
+using namespace std;
+
+int t;
+int s;
+
+int main() {
+    while(scanf("%d", &t) != 0) {
+        printf("Scenario #%d\n", s++);
+        for(int i = 0; i < t; i++) {
+            int num;
+            scanf("%d", &num);
+            for(int j = 0; j < num; j++) {
+
+            }
+        }
+    }
+}
